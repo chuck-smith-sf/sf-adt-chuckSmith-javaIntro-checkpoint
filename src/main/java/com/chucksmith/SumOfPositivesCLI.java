337ea40eae0887acf32cae49a8de1d188fa85f6e
cjsmith33 <charleswsmith8@gmail.com>
2021-07-20 08:21:33 -0700

    added sum logic and tested. App is working as expected

--- src/main/java/com/chucksmith/SumOfPositivesCLI.java
+++ src/main/java/com/chucksmith/SumOfPositivesCLI.java
@@ -3,6 +3,10 @@ package com.chucksmith;
 public class SumOfPositivesCLI {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+       int sum = 0;
+           for (int i = 0; i < args.length; i++) {
+               if (Integer.parseInt(args[i]) > 0) sum += Integer.parseInt(args[i]);
+           }
+       System.out.println(sum);
     }
 }
